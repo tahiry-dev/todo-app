a3aaa2bb4bd063a50b4ad95b7e6467166223bde7
tahiry-dev <tahirycontact@gmail.com>
2021-02-11 12:58:37 +0300

    project initialization

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/style.css';
+
+import basic from './components/basic'
+
+const basicPage = () => {
+    const parent = document.createElement('div');
+    parent.innerHTML = `
+          ${basic().outerHTML}
+      `;
+    return parent;
+};
+
+document.getElementById('wrapper').appendChild(basicPage())
